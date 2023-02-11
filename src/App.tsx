5c1ed134b2879485a850a656e798c0874a32bf5e
zemosamurai <zemosamurai@gmail.com>
2023-02-11 16:27:47 +0300

    commit - 3

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Apps">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
