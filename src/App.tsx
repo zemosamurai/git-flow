ce992a21de466d6d23dd5123670ed3ad23fb5e49
zemosamurai <zemosamurai@gmail.com>
2023-02-11 17:59:03 +0300

    commit - 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="Apps">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blankssss"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">
+      <div>Hello</div>
+      <div>MY</div>
+      <div>WORLD</div>
     </div>
   );
 }
