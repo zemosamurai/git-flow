7c9dfbc6f1376c7db769935a536f460de752c324
zemotest <kimharokhau@gmail.com>
2023-02-12 13:37:39 +0300

    DEV-2?

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div>Hello</div>
-      <div>MY</div>
-      <div>WORLD</div>
-      <div>WORLD</div>
-      <div>developer/popup branch commit 1</div>
-      <div>developer/popup branch commit 2</div>
-      <div>developer/login branch commit 1</div>
-      <div>developer/login branch commit 2</div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>Hello</div>
+            <div>MY</div>
+            <div>WORLD</div>
+            <div>WORLD</div>
+            <div>developer/popup branch commit 1</div>
+            <div>developer/popup branch commit 2</div>
+            <div>developer/login branch commit 1</div>
+            <div>developer/login branch commit 2</div>
+            <div>
+                <h1>DEV-2?</h1>
+            </div>
+        </div>
+    );
 }
 
 export default App;
