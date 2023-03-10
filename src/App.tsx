c056ad0cf9cbc19da05a9ab8dfbda33698fbaaba
zemosamurai <zemosamurai@gmail.com>
2023-03-10 09:07:12 +0300

    developer/popup branch commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <div>Hello</div>
-            <div>MY</div>
-            <div>WORLD</div>
-            <div>WORLD</div>
-            <div>developer/popup branch commit 1</div>
-            <div>developer/popup branch commit 2</div>
-            <div>developer/login branch commit 1</div>
-            <div>developer/login branch commit 2</div>
+            <>BEKA</>
             <div>
                 <h1>DEV-2?</h1>
                 <h1>DEV-2!!!</h1>
